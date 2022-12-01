a13103d3de9de9602e657367688b8f7bab317108
Anthony Garcia <44326005+aedwardg@users.noreply.github.com>
2022-12-01 13:55:37 -0800

    shorten 2022 01_golfed

--- 2022/01_golfed.rb
+++ 2022/01_golfed.rb
@@ -2,5 +2,4 @@ File
   .read('./inputs/01.txt')
   .split("\n\n")
   .map { |s| s.split("\n").map(&:to_i).sum }
-  .tap { p _1.max }
-  .then { p _1.max(3).sum }
+  .then { p [_1.max, _1.max(3).sum] }
